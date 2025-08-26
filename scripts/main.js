01254370b7ebb9dad0f4b4b720ae9859baa4a251
Fenna Kort <fenna.kort@tutanota.com>
2025-08-26 09:50:27 -0600

    #7 replaced use of "menu" in id's, variables, and comments --- better reflects that the navigation is not a programmatic menu

--- scripts/main.js
+++ scripts/main.js
@@ -6,60 +6,60 @@ import {
 const topnavOpen = document.querySelector('#topnavOpen')
 const topnavClose = document.querySelector('#topnavClose')
 const isMobile = window.matchMedia('(width < 50em)');
-const topnavMenu = document.querySelector('#topnavMenu');
+const topnav = document.querySelector('#topnav');
 const body = document.querySelector('body');
 const main = document.querySelector('main');
 
-function setupMobileMenu(isMobile){
+function setupMobileNav(isMobile){
 	if (isMobile.matches){
 		// is mobile
 		console.log('is mobile');
-		closeMobileMenu();
-		// topnavMenu.setAttribute('inert', '');
-		topnavMenu.style.transition = 'none';
+		closeMobileNav();
+		// topnav.setAttribute('inert', '');
+		topnav.style.transition = 'none';
 	}
 	else {
 		// is tablet/desktop
 		console.log('is tablet/desktop');
-		// closeMobileMenu();
-		topnavMenu.removeAttribute('inert');
+		// closeMobileNav();
+		topnav.removeAttribute('inert');
 	}
 }
 
-function openMobileMenu(){
+function openMobileNav(){
 	topnavOpen.setAttribute('aria-expanded', 'true');
-	topnavMenu.removeAttribute('inert'); 
-	topnavMenu.removeAttribute('style');
+	topnav.removeAttribute('inert'); 
+	topnav.removeAttribute('style');
 	main.setAttribute('inert', ''); 
 	disableBodyScroll(body);
 	topnavClose.focus();
 }
 
-function closeMobileMenu(){
+function closeMobileNav(){
 	topnavOpen.setAttribute('aria-expanded', 'false');
-	topnavMenu.setAttribute('inert', ''); //i think there was something in the video about making a separate wrapper for the mobile menu - maybe the fact that we're supposed to be setting inert on it is likely the reason? 
+	topnav.setAttribute('inert', ''); //i think there was something in the video about making a separate wrapper for the mobile Nav - maybe the fact that we're supposed to be setting inert on it is likely the reason? 
 		// not sure that would make a difference because wouldn't setting inert on a wrapper element still cause its content to be inert?
-	// rn this is setting inert on the nav element as a whole whenever the mobile menu is not open - inert is being set on main when mobile menu IS open, which is intended behaviour for mobile menu being open 
+	// rn this is setting inert on the nav element as a whole whenever the mobile Nav is not open - inert is being set on main when mobile Nav IS open, which is intended behaviour for mobile Nav being open 
 	main.removeAttribute('inert');
 	enableBodyScroll(body);
 	topnavOpen.focus();
 
-	setTimeout(() => { // i think this isn't finished because the browser console gives an error that siteTimeout isn't defined. BUT even with this error, it IS functioning to ensure the mobile menu isn't shown when changing breakpoints. 
+	setTimeout(() => { // i think this isn't finished because the browser console gives an error that siteTimeout isn't defined. BUT even with this error, it IS functioning to ensure the mobile Nav isn't shown when changing breakpoints. 
 	// oh lol it's just supposed to be a function called "setTimeout" but I'd dictated "siteTimeout"
-		topnavMenu.style.transition = 'none';
+		topnav.style.transition = 'none';
 	}, 500);
 }
 
 
 
 
-setupMobileMenu(isMobile);
+setupMobileNav(isMobile);
 
-isMobile.addEventListener('deviceorientation', setupMobileMenu(isMobile));
+isMobile.addEventListener('deviceorientation', setupMobileNav(isMobile));
 
-topnavOpen.addEventListener('click', openMobileMenu);
-topnavClose.addEventListener('click', closeMobileMenu);
+topnavOpen.addEventListener('click', openMobileNav);
+topnavClose.addEventListener('click', closeMobileNav);
 
 isMobile.addEventListener('change', function(e){
-	setupMobileMenu(e); //event fires any time isMobile's breakpoint is crossed; currently not working correctly - 21 aug - what did I mean by this lol???
+	setupMobileNav(e); //event fires any time isMobile's breakpoint is crossed; currently not working correctly - 21 aug - what did I mean by this lol???
 });
